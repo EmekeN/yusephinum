ff01cada046cc6e52fe3425fee4c619e8e57d382
Emeke <emeke@dumpling.us>
2022-12-06 00:52:08 -0800

    add audioplayer

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,56 +16,70 @@ const IndexPage = () => {
   const compassRef = useRef();
   const galleryRef = useRef();
   const aboutRef = useRef();
-  const loreRef = useRef();
+  const exhibitRef = useRef();
   const storyLinkRef = useRef();
-  const [mounted, setMounted] = useState(false)
-  const timelineRef = useRef()
-  useEffectOnlyOnce(() =>{setMounted(true)
-    storyRef.current.classList.remove("visually-hidden")
-  },[])
+  const [mounted, setMounted] = useState(false);
+  const timelineRef = useRef();
+  useEffectOnlyOnce(() => {
+    setMounted(true);
+    storyRef.current.classList.remove("visually-hidden");
+  }, []);
 
-  
-    gsap.registerPlugin(SplitText);
-    if(mounted) {
+  gsap.registerPlugin(SplitText);
+  if (mounted) {
+    timelineRef.current = gsap?.timeline({ defaults: { ease: "power1" } });
+    let mySplitText = new SplitText(storyRef.current, { type: "words, chars", ease: "Sine.in" });
+    mySplitText.split({ type: "chars, words, lines" });
 
-      timelineRef.current = gsap?.timeline({ defaults: { ease: "power1" } });
-      let mySplitText = new SplitText(storyRef.current, { type: "words, chars", ease: "Sine.in" });
-      mySplitText.split({ type: "chars, words, lines" });
-  
-      //prettier-ignore
-      timelineRef.current
+    //prettier-ignore
+    timelineRef.current
         .from(mySplitText.words, {opacity: 0, duration: 2, scale: 0, autoAlpha: 0, y: "-50%", force3D: true, stagger: 0.02, filter: "blur(.5rem)"})
       .to(mySplitText.chars, {opacity: 0, duration: 1, stagger: ".05",}, 4)
       .to([compassRef.current], {x: 0, opacity: 1, rotate: 360, duration: 1, delay: .15, ease: "Sine.in"},)
-      .to([galleryRef.current,storyLinkRef.current, aboutRef.current, loreRef.current, ], {opacity: 1, stagger: .5, duration: .25});
-    }
+      .to([galleryRef.current,storyLinkRef.current, aboutRef.current, exhibitRef.current, ], {opacity: 1, stagger: .5, duration: .25});
+  }
 
-    const handleKillAnimation = () => {
-      if(timelineRef.current) {
-        timelineRef.current.seek(timelineRef.current.endTime() - 2)
-      }
+  const handleKillAnimation = () => {
+    if (timelineRef.current) {
+      timelineRef.current.seek(timelineRef.current.endTime() - 2);
     }
+  };
 
-    const handleKeyPress = (e) => {
-      if(e.key === "Enter") {
-        e.preventDefault()
-        handleKillAnimation()
-      }
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleKillAnimation();
     }
-  
+  };
+
+  const AudioPlayer = (url) => {
+    return (
+      <div className="AudioPlayer" style={{ opacity: "0" }}>
+        <audio controls width className="player">
+          <source src="https://yusephinum-web-assets.s3.us-west-2.amazonaws.com/Yusephinum-5.mp3" />
+          Sorry you can't hear this
+        </audio>
+      </div>
+    );
+  };
 
   return (
-    <div className="Home" onClick={handleKillAnimation} onKeyPress={(e) => handleKeyPress(e)} >
+    <div className="Home" onClick={handleKillAnimation} onKeyPress={(e) => handleKeyPress(e)}>
       <Seo
         title={"Welcome to Yusephinum"}
         description="An episodic world building experience told through interdisciplinary installations that explore the complexity of existence and finding a place to call home."
       />
       <section className="hero-story" onKeyPress={(e) => handleKeyPress(e)}>
-        <h1 ref={storyRef} className="title visually-hidden" style={{zIndex: 0}}>Welcome to a strange new dimension.</h1>
+        <h1 ref={storyRef} className="title visually-hidden" style={{ zIndex: 0 }}>
+          Welcome to a strange new dimension.
+        </h1>
 
         <div className="link-ctn">
           <div ref={aboutRef} className="planet-link about">
             <Link to="/about">About</Link>
+          </div>
+          <div ref={exhibitRef} className="planet-link exhibit">
+            <Link to="/about">Current Exhibit</Link>
           </div>
           <div ref={galleryRef} className="planet-link gallery">
             <a
@@ -78,12 +92,15 @@ const IndexPage = () => {
             </a>
           </div>
           <div ref={storyLinkRef} className="planet-link story">
-            <a href="https://yusephinum.myshopify.com/" target="_blank" rel="noopener noreferrer">Store</a>
+            <a href="https://yusephinum.myshopify.com/" target="_blank" rel="noopener noreferrer">
+              Store
+            </a>
           </div>
           {/* <div ref={loreRef} className="planet-link lore">
             <Link to="/lore">Lore</Link>
           </div> */}
           <img src={CompassLogo} ref={compassRef} alt="Yusephinum" className="center-logo" />
+          <AudioPlayer url="https://emeken-portfolio.s3.us-west-2.amazonaws.com/web/audio/611+Harvard+Ave+E.mp3"></AudioPlayer>
         </div>
       </section>
     </div>
